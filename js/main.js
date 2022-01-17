1f08a7d87d2bd305f128e1d260781826fa543c72
Rasool Hashbari <Rasool.hashbari@gmail.com>
2022-01-17 12:32:44 +0330

    Footer

--- js/main.js
+++ js/main.js
@@ -1,18 +1,37 @@
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        rtl:true,
-        loop:true,
-        margin:10,
-        nav:false,
-        responsive:{
-            0:{
-                items:2
+$(document).ready(function() {
+    new WOW().init();
+    $('.prod').owlCarousel({
+        rtl: true,
+        loop: true,
+        margin: 10,
+        nav: false,
+        responsive: {
+            0: {
+                items: 2
             },
-            600:{
-                items:3
+            600: {
+                items: 3
             },
-            1000:{
-                items:4
+            1000: {
+                items: 4
+            }
+        }
+    })
+
+    $('.prod1').owlCarousel({
+        rtl: true,
+        loop: true,
+        margin: 10,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            800: {
+                items: 2
+            },
+            1200: {
+                items: 3
             }
         }
     })
